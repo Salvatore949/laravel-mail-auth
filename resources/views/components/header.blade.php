ecb6c948464cb864dcad81e17b3597d348acb9f4
Salvatore949 <salvo.mess49@gmail.com>
2022-02-15 23:00:15 +0100

    login,logout insert

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,45 @@
 <header>
+<h1> FORM VIDEOGAMES</h1>
 
+@auth
+
+<a href="{{route('logout')}}"> LOGOOUT </a>
+
+
+@else
+
+<form action="{{route('login')}}" method="POST">
+
+    @method('POST')
+    @csrf
+    <label for="email">Email</label><br>
+    <input type="email" name="email"><br>
+    <label for="password">Password</label><br>
+    <input type="password" name="password"><br>
+
+    <input type="submit" value="Login"><br>
+    
+</form>
+
+<form action="{{route('register')}}" method="POST">
+
+         @method('POST')
+         @csrf
+
+        <label for="name">Name</label><br>
+        <input type="text" name="name"><br>
+        <label for="email">Email</label><br>
+        <input type="email" name="email"><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password"><br>
+        <label for="password_confirmation">Password-confirmation</label><br>
+        <input type="password" name="password_confirmation"><br>
+        <br>
+        <input type="submit" value= "Register">
+    </form>
+
+
+@endauth
 
 
 </header>
